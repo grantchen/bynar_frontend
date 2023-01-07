b39151ce44bbebca79546aafceb68b26f97d440a
Asad <asad.baloch9@gmail.com>
2023-01-07 15:36:10 +0500

    test changed

--- tests/test.js
+++ tests/test.js
@@ -4,27 +4,12 @@ var test = require('unit.js');
 var index = require('../index.js');
 var api = require('../api.js');
 
-describe('Tests index', function() {
-  it('verifies successful response', function(done) {
-    index.get({ /* event */ }, { /* context */ }, (err, result) => {
-      try {
-        test.number(result.statusCode).is(200);
-        test.string(result.body).contains('Congratulations');
-        test.value(result).hasHeader('content-type', 'text/html');
-        done();
-      } catch(error) {
-        done(error);
-      }
-    });
-  });
-});
-
-// describe('Tests api', function() {
+// describe('Tests index', function() {
 //   it('verifies successful response', function(done) {
-//     api.get({ /* event */ }, { /* context */ }, (err, result) => {
+//     index.get({ /* event */ }, { /* context */ }, (err, result) => {
 //       try {
 //         test.number(result.statusCode).is(200);
-//         // test.string(result.body).contains('Congratulations');
+//         test.string(result.body).contains('Congratulations');
 //         test.value(result).hasHeader('content-type', 'text/html');
 //         done();
 //       } catch(error) {
@@ -33,3 +18,18 @@ describe('Tests index', function() {
 //     });
 //   });
 // });
+
+describe('Tests api', function() {
+  it('verifies successful response', function(done) {
+    api.get({ /* event */ }, { /* context */ }, (err, result) => {
+      try {
+        test.number(result.statusCode).is(200);
+        // test.string(result.body).contains('Congratulations');
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+});
