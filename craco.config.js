5e9c68fb3ebfc09f236fb6724e7c8e27f026e752
suyash jai <suyash.jai@remotestate.com>
2023-05-06 00:53:58 +0530

    fix-build

--- craco.config.js
+++ craco.config.js
@@ -7,10 +7,6 @@ module.exports={
             process: {env: {}}
           })
         ]
-      },
-      fallback: {
-        'react/jsx-runtime': 'react/jsx-runtime.js',
-        'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
-      },
+      }
     }
   }
