df8f50faea153b2d8da3d0a262b9764a3838864c
suyashjai <suyash.jai@remotestate.com>
2023-05-17 08:10:34 +0530

    instant theme selection

--- src/sdk/new-theme/ThemePreferenceContext.js
+++ src/sdk/new-theme/ThemePreferenceContext.js
@@ -54,7 +54,7 @@ function ThemePreferenceProvider({ children }) {
     }, [user, isSystemThemeDark]);
 
     useEffect(() => {
-        if(!user || !theme){
+        if(!theme){
             return
         }
         document.documentElement.setAttribute(
