7c62ee1bc42c55647a746a969ea132b4f7e3e655
suyash jai <suyash.jai@remotestate.com>
2023-05-19 19:13:27 +0530

    User profile icon should be Initals or profile picture.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,6 +9,7 @@ import {
   Popover,
   PopoverContent,
 } from "@carbon/react";
+import { UserProfileImage } from "@carbon/ibm-products";
 import { Search20, UserAvatar20, UserData20 } from "@carbon/icons-react";
 import { Navbar } from "../Navbar";
 import { Outlet } from "react-router-dom";
@@ -18,6 +19,8 @@ import HeaderTab from "./HeaderTab";
 import {
   LanguageModal,
   useUserManagement,
+  useThemePreference,
+  useAuth
 } from "../../sdk";
 import { TearSheets } from "../TearSheet";
 import { useSearchParams } from "react-router-dom";
@@ -33,9 +36,12 @@ export const CommonHeader = () => {
 const HeaderComponent = ({ isSideNavExpanded, onClickSideNavExpand }) => {
   const [searchParams, setSearchParams] = useSearchParams();
   //todo for Suyash: get token from auth context
-  
+
   const [profileDropdown, setProfileDropdown] = useState(false);
-  
+  const {theme } = useThemePreference();
+
+  const {user} = useAuth();
+
   const wrapperRef = useRef(null);
   const showProfilePanelRef = useRef(null);
   const setShowProfilePanelRef = useRef(null);
@@ -43,7 +49,7 @@ const HeaderComponent = ({ isSideNavExpanded, onClickSideNavExpand }) => {
   setShowProfilePanelRef.current = setProfileDropdown;
   const { t } = useTranslation();
 
-  const {isUserManagementAllowed} = useUserManagement()
+  const { isUserManagementAllowed } = useUserManagement()
 
   const handleDropDown = (e) => {
     e.preventDefault();
@@ -69,86 +75,96 @@ const HeaderComponent = ({ isSideNavExpanded, onClickSideNavExpand }) => {
   const [openLanguageModal, setLanguageModalOpen] = useState(false);
 
   const {isUserListOpen, setIsUserListOpen} = useMemo(() => ({
-    isUserListOpen: searchParams.get('isUserListOpen') === 'true', 
+    isUserListOpen: searchParams.get('isUserListOpen') === 'true',
     setIsUserListOpen: (shouldOpen) => setSearchParams({isUserListOpen: shouldOpen})
   }), [searchParams.get('isUserListOpen')])
 
   return (
     <>
-        <Header
-          aria-label="Bynar"
-          className="header-container"
+      <Header
+        aria-label="Bynar"
+        className="header-container"
+      >
+        <HeaderMenuButton
+          aria-label="Open menu"
+          isCollapsible
+          onClick={onClickSideNavExpand}
+          isActive={isSideNavExpanded}
+        />
+        <div
+          className={
+            isSideNavExpanded
+              ? "header-container-box"
+              : "header-box-dashboard"
+          }
         >
-          <HeaderMenuButton
-            aria-label="Open menu"
-            isCollapsible
-            onClick={onClickSideNavExpand}
-            isActive={isSideNavExpanded}
-          />
-          <div
-            className={
-              isSideNavExpanded
-                ? "header-container-box"
-                : "header-box-dashboard"
-            }
-          >
-            <HeaderName className="heading-content" prefix={t("")}>
-              {t("Bynar")}
-            </HeaderName>
-            <HeaderName className="heading-content-new" prefix={t("")}>
-              {t("platform")}
-            </HeaderName>
-            <HeaderTab />
-            {/* <button style={{ cursor: 'pointer' }} onClick={handleOpenModalClick}>user</button> */}
-            <HeaderGlobalBar className="header-tab">
-              {isUserManagementAllowed && (
-                <HeaderGlobalAction
-                  aria-label={t('user')}
-                  onClick={() => setIsUserListOpen(true)}
-                >
-                  <UserData20/>
-                </HeaderGlobalAction>
-              )}
-              <HeaderGlobalAction aria-label={t('search')} onClick={() => { }}>
-                <Search20 />
+          <HeaderName className="heading-content" prefix={t("")}>
+            {t("Bynar")}
+          </HeaderName>
+          <HeaderName className="heading-content-new" prefix={t("")}>
+            {t("platform")}
+          </HeaderName>
+          <HeaderTab />
+          {/* <button style={{ cursor: 'pointer' }} onClick={handleOpenModalClick}>user</button> */}
+          <HeaderGlobalBar className="header-tab">
+            {isUserManagementAllowed && (
+              <HeaderGlobalAction
+                aria-label={t('user')}
+                onClick={() => setIsUserListOpen(true)}
+              >
+                <UserData20/>
               </HeaderGlobalAction>
-              {/* <HeaderGlobalAction aria-label="Notifications" onClick={() => setOpen(!open)}>
+            )}
+            <HeaderGlobalAction aria-label={t('search')} onClick={() => { }}>
+              <Search20 />
+            </HeaderGlobalAction>
+            {/* <HeaderGlobalAction aria-label="Notifications" onClick={() => setOpen(!open)}>
             <Notification20 />
           </HeaderGlobalAction> */}
 
-              <div ref={wrapperRef}>
-                <>
-                  <Popover
-                    align="bottom-right"
-                    caret={false}
-                    open={profileDropdown}
+            <div ref={wrapperRef}>
+              <>
+                <Popover
+                  align="bottom-right"
+                  caret={false}
+                  open={profileDropdown}
+                >
+                  <HeaderGlobalAction
+                    className="header-tab"
+                    aria-label={t('user')}
+                    onClick={handleDropDown}
                   >
-                    <HeaderGlobalAction
-                      className="header-tab"
-                      aria-label={t('user')}
-                      onClick={handleDropDown}
-                    >
-                      <UserAvatar20 />
-                    </HeaderGlobalAction>
-                    <PopoverContent>
-                      <ProfileDropdown
-                        openLanguageModal={openLanguageModal}
-                        setLanguageModalOpen={setLanguageModalOpen}
-                      />
-                    </PopoverContent>
-                  </Popover>
-                </>
-              </div>
-            </HeaderGlobalBar>
-          </div>
-          <Navbar isSideNavExpanded={isSideNavExpanded} onClickSideNavExpand={onClickSideNavExpand}/>
-        </Header>
-        <Outlet />
+                    <UserProfileImage
+                      backgroundColor={"light-cyan"}
+                      size={"md"}
+                      initials={user?.fullName}
+                      tooltipText={user?.fullName }
+                      theme={
+                        theme === "g90"
+                          ? "dark"
+                          : "light"
+                      }
+                    />
+                  </HeaderGlobalAction>
+                  <PopoverContent>
+                    <ProfileDropdown
+                      openLanguageModal={openLanguageModal}
+                      setLanguageModalOpen={setLanguageModalOpen}
+                    />
+                  </PopoverContent>
+                </Popover>
+              </>
+            </div>
+          </HeaderGlobalBar>
+        </div>
+        <Navbar isSideNavExpanded={isSideNavExpanded} onClickSideNavExpand={onClickSideNavExpand}/>
+      </Header>
+      <Outlet />
 
-        <LanguageModal
-          openLanguageModal={openLanguageModal}
-          setLanguageModalOpen={setLanguageModalOpen}
-        />
+      <LanguageModal
+        openLanguageModal={openLanguageModal}
+        setLanguageModalOpen={setLanguageModalOpen}
+      />
 
       <TearSheets setIsOpen={setIsUserListOpen} isOpen={isUserListOpen} />
     </>
