bef99a3094ef2d496755ca54b7ef778a89783893
suyashjai <suyash.jai@remotestate.com>
2023-05-29 17:42:09 +0530

    theme back to g10

--- src/components/Dashboard/DashboardContainer.js
+++ src/components/Dashboard/DashboardContainer.js
@@ -8,25 +8,31 @@ import { ViewUsageCard } from "../Cards/ViewUsageCard/ViewUsageCard";
 import { SupportCard } from "../Cards/SupportCard/SupportCard";
 import { useTranslation } from "react-i18next";
 import { Add20 } from "@carbon/icons-react";
-import {useMobile} from "../../sdk";
+import { useMobile } from "../../sdk";
+import { ExpressiveCard } from "@carbon/ibm-products";
 
 const DashboardContainer = () => {
-  const { t } = useTranslation();
-  const isMobile = useMobile()
+    const { t } = useTranslation();
+    const isMobile = useMobile();
 
     return (
-    <div className="dashboard-box">
-      <div className="bynar-heading">
-        <Heading className="heading">{t("header")}</Heading>
-        <Button renderIcon={Add20} hasIconOnly={isMobile ? true : false}>{t("create-resource-button")}</Button>
-      </div>
-      <div className="dashboard-container-box">
-        <NewsInfoCard />
-        <ViewUsageCard />
-        <SupportCard style={{marginRight:"0px",}} />
-      </div>
-    </div>
-  );
+        <div className="dashboard-box">
+            <div className="bynar-heading">
+                <Heading className="heading">{t("header")}</Heading>
+                <Button
+                    renderIcon={Add20}
+                    hasIconOnly={isMobile ? true : false}
+                >
+                    {t("create-resource-button")}
+                </Button>
+            </div>
+            <div className="dashboard-container-box">
+                <NewsInfoCard />
+                <ViewUsageCard />
+                <SupportCard style={{ marginRight: "0px" }} />
+            </div>
+        </div>
+    );
 };
 
 export default DashboardContainer;
