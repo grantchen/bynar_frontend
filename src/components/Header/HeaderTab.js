b5da6d0dc7a8aea2f28302dd08ee962b7e2833ea
xander <xandercheung0@gmail.com>
2023-10-08 14:04:40 +0800

    fix(header): do not refresh tab content when navigate to opened tab

--- src/components/Header/HeaderTab.js
+++ src/components/Header/HeaderTab.js
@@ -1,12 +1,11 @@
-import React, {useRef, useContext} from "react";
-import {Button, TabList, Tabs, Tab} from "@carbon/react";
+import React, { useRef, useContext } from "react";
+import { Button, TabList, Tabs, Tab } from "@carbon/react";
 import { Add20 } from '@carbon/icons-react';
 import "./HeaderTab.scss";
-import {TabContext, useMobile} from "../../sdk";
-import {useTranslation} from "react-i18next";
+import { TabContext } from "../../sdk";
 
 const HeaderTab = ({ className }) => {
-    const {tab, handleAddTab, handleRemoveTab, activeTab, setActiveTab} =
+    const { tab, handleAddTab, handleRemoveTab, activeTab, setActiveTab } =
         useContext(TabContext);
     const carouselRef = useRef(null);
 
@@ -20,15 +19,17 @@ const HeaderTab = ({ className }) => {
 
     return (
         <>
-            <div className={ `tab ${className}` }>
-                <div className="tab-buttons-list" ref={carouselRef}>
-                    <div style={{display: "flex", whiteSpace: "nowrap", height: "100%"}}>
-                        <Tabs selectedIndex={activeTab} onChange={handleTabChange} dismissable onTabCloseRequest={removeTab}>
+            <div className={ `tab ${ className }` }>
+                <div className="tab-buttons-list" ref={ carouselRef }>
+                    <div style={ { display: "flex", whiteSpace: "nowrap", height: "100%" } }>
+                        <Tabs selectedIndex={ activeTab } onChange={ handleTabChange } dismissable
+                              onTabCloseRequest={ removeTab }>
                             <TabList aria-label="List of tabs">
-                                {tab.map((item, index) =>
-                                    <Tab key={index} className={item.isDelted ? 'custom-tab' : 'custom-tab tab-stable'}>
-                                        {item.label}
-                                    </Tab>)}
+                                { tab.map((item, index) =>
+                                    <Tab key={ index }
+                                         className={ item.isDelted ? 'custom-tab' : 'custom-tab tab-stable' }>
+                                        { item.label }
+                                    </Tab>) }
                             </TabList>
                         </Tabs>
                     </div>
@@ -38,7 +39,7 @@ const HeaderTab = ({ className }) => {
                     kind="ghost"
                     className="add-new-tab"
                     hasIconOnly
-                    onClick={(e) => handleAddTab()}
+                    onClick={ (e) => handleAddTab() }
                 >
                     <Add20 aria-label="Add" />
                 </Button>
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,22 @@
 import { useContext } from "react";
 import { TabContext } from "../../sdk";
 import './Dashboard.scss'
+import { TabPanel, TabPanels, Tabs } from "@carbon/react";
 
 const Dashboard = () => {
-  const { tab, activeTab } = useContext(TabContext);
-  const tabContent = tab?.[activeTab]?.content;
-  return (
-    <div
-      className="dashboard-container"
-    >
-      {tabContent}
-    </div>
-  );
+    const { tab, activeTab } = useContext(TabContext);
+    return (
+        <div className="dashboard-container">
+            <Tabs selectedIndex={ activeTab }>
+                <TabPanels>
+                    {
+                        tab.map(tab => {
+                            return <TabPanel className="content-wrapper">{ tab.content }</TabPanel>
+                        })
+                    }
+                </TabPanels>
+            </Tabs>
+        </div>
+    );
 };
 export default Dashboard
