5edfe47cce074c5b70600eee7f6374c032a67139
xander <xandercheung0@gmail.com>
2023-10-10 15:58:53 +0800

    fix(header tab): remove unused div

--- src/components/Header/HeaderTab.js
+++ src/components/Header/HeaderTab.js
@@ -139,9 +139,7 @@ const HeaderTab = ({ className }) => {
                                                         removeTab(item.id);
                                                     } }
                                                 >
-                                                    <div>
-                                                        <Close16 />
-                                                    </div>
+                                                    <Close16 />
                                                 </Button>
                                             )
                                         }
