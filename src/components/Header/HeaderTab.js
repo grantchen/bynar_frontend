a0c7db8c25f0605fc5503d5f80caf70753bf7d38
xander <xandercheung0@gmail.com>
2023-10-10 14:21:16 +0800

    feat(header tab): drop-down tab list searh

--- src/components/Header/HeaderTab.js
+++ src/components/Header/HeaderTab.js
@@ -1,5 +1,15 @@
 import React, { useRef, useContext, useState, useEffect } from "react";
-import { Button, TabList, Tabs, Tab, ContainedList, ContainedListItem, PopoverContent, Popover } from "@carbon/react";
+import {
+    Button,
+    TabList,
+    Tabs,
+    Tab,
+    ContainedList,
+    ContainedListItem,
+    PopoverContent,
+    Popover,
+    Search
+} from "@carbon/react";
 import { Add20 } from '@carbon/icons-react';
 import "./HeaderTab.scss";
 import { TabContext } from "../../sdk";
@@ -14,16 +24,33 @@ const HeaderTab = ({ className }) => {
     const carouselRef = useRef(null);
     const dropdownTabsRef = useRef(null);
     const [isDropdownTabsOpen, setIsDropdownTabsOpen] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const handleChange = event => {
+        setSearchTerm(event.target.value);
+    };
 
     const handleTabChange = (evt) => {
-        if (tab[evt.selectedIndex]?.loaded === true){
+        if (tab[evt.selectedIndex]?.loaded === true) {
             setActiveTab(evt.selectedIndex);
         }
     };
 
-    const removeTab = (index) => {
-        handleRemoveTab(tab[index]?.id, index);
+    const handleTabListChange = (tabId) => {
+        const index = tab.findIndex((item) => item.id === tabId);
+        if (tab[index]?.loaded === true) {
+            setActiveTab(index);
+        }
     };
+
+    const removeTab = (tabId) => {
+        handleRemoveTab(tabId);
+    };
+
+    useEffect(() => {
+        const results = tab.filter(listItem => listItem.label.toLowerCase().includes(searchTerm.toLowerCase()));
+        setSearchResults(results);
+    }, [searchTerm, tab]);
 
     useEffect(() => {
         const handleClickDropdownTabsOutside = (event) => {
@@ -47,15 +74,17 @@ const HeaderTab = ({ className }) => {
                 <div className="tab-buttons-list" ref={ carouselRef }>
                     <div style={ { display: "flex", whiteSpace: "nowrap", height: "100%" } }>
                         <Tabs selectedIndex={ activeTab } onChange={ handleTabChange } dismissable
-                              onTabCloseRequest={ removeTab }>
+                              onTabCloseRequest={ (index) => {
+                                  removeTab(tab[index].id)
+                              } }>
                             <TabList aria-label="List of tabs">
                                 { tab.map((item, index) =>
-                                        <Tab key={ index }
-                                             className={ item.isDelted ? 'custom-tab' : 'custom-tab tab-stable' }>
-                                            {
-                                                item.loaded ? (item.label) : (<TabSkeleton></TabSkeleton>)
-                                            }
-                                        </Tab>
+                                    <Tab key={ index }
+                                         className={ item.isDelted ? 'custom-tab' : 'custom-tab tab-stable' }>
+                                        {
+                                            item.loaded ? (item.label) : (<TabSkeleton></TabSkeleton>)
+                                        }
+                                    </Tab>
                                 ) }
                             </TabList>
                         </Tabs>
@@ -84,36 +113,43 @@ const HeaderTab = ({ className }) => {
                         <ChevronDown20 />
                     </Button>
                     <PopoverContent className="header-dynamic-dropdown-tabs-content">
-                        <ContainedList
-                            label={ t('opened-tabs') }
-                            size="md"
-                        >
-                            { tab.map((item, index) =>
-                                <ContainedListItem
-                                    key={ index }
-                                    className={activeTab === index ? 'list-item-active' : ''}
-                                    action={
-                                        item.isDelted && (
-                                            <Button
-                                                kind="ghost"
-                                                hasIconOnly
-                                                iconDescription={ t('close') }
-                                                onClick={ () => {
-                                                    removeTab(index);
-                                                } }
-                                            >
-                                                <Close20 />
-                                            </Button>
-                                        )
-                                    }
-                                    onClick={ () => {
-                                        handleTabChange({selectedIndex:index});
-                                        setIsDropdownTabsOpen(false);
-                                    } }
-                                >{ item.label }
-                                </ContainedListItem>
-                            ) }
-                        </ContainedList>
+                        <div className="list-wrapper">
+                            <ContainedList
+                                label={ t("opened-tabs") }
+                                size="md"
+                            >
+                                <Search placeholder={ t("filter") }
+                                        value={ searchTerm }
+                                        onChange={ handleChange }
+                                        closeButtonLabelText={ t("clear") }
+                                        size="md" />
+                                { searchResults.map((item, index) =>
+                                    <ContainedListItem
+                                        key={ index }
+                                        className={ tab[activeTab].id === item.id ? 'list-item-active' : '' }
+                                        action={
+                                            item.isDelted && (
+                                                <Button
+                                                    kind="ghost"
+                                                    hasIconOnly
+                                                    title={ t("close") }
+                                                    onClick={ () => {
+                                                        removeTab(item.id);
+                                                    } }
+                                                >
+                                                    <Close20 />
+                                                </Button>
+                                            )
+                                        }
+                                        onClick={ () => {
+                                            handleTabListChange(item.id);
+                                            setIsDropdownTabsOpen(false);
+                                        } }
+                                    >{ item.label }
+                                    </ContainedListItem>
+                                ) }
+                            </ContainedList>
+                        </div>
                     </PopoverContent>
                 </Popover>
             </div>
