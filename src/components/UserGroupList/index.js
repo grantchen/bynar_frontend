ee816befb2d8033b6cd5a1a9de875dc12e76c7f8
xander <xandercheung0@gmail.com>
2023-10-19 14:01:50 +0800

    fix(user groups): disable adding rows to grouped root row

--- src/components/UserGroupList/index.js
+++ src/components/UserGroupList/index.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./UserGroupList.scss";
 import { TreeGrid } from "../TreeGrid";
+import { TabContext } from "../../sdk";
 
 const UserGroupList = ({ tabId }) => {
+    const { tab, activeTab } = useContext(TabContext);
+
+    useEffect(() => {
+        if (tab[activeTab].id !== tabId) return;
+
+        // Called on adding row
+        window.Grids.OnCanRowAdd = function (G, par, next) {
+            if (G.Editing === 2) return false;
+            // Disable adding rows to grouped root row
+            if (par.Level === 0 && par.Def?.Name === "Group" && par.Def?.CDef === "R") return false;
+            return
+        }
+
+        return () => {
+            window.Grids.OnCanRowAdd = null;
+        }
+    }, [tab, activeTab]);
+
     return (
         <>
             <div className="tree-grid-content">
