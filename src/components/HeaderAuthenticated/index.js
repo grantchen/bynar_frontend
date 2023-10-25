2b9257dafea620958f17d72509376960af15ae6a
wilson_wz <wilson_wz@163.com>
2023-10-25 19:41:53 +0800

    Optimize code

--- src/components/HeaderAuthenticated/index.js
+++ src/components/HeaderAuthenticated/index.js
@@ -140,13 +140,6 @@ function _AuthenticatedAppHeader({ isSideNavExpanded, onClickSideNavExpand }) {
                         aria-label={ user?.fullName ?? t("user") }
                         onClick={ () => {
                             setIsProfileDropdownOpen(!isProfileDropdownOpen);
-                            const popoverContent = document.querySelector('.custom-popover-content');
-                            console.log(popoverContent.style.maxWidth)
-                            if (popoverContent.style.maxWidth === '23rem') {
-                                popoverContent.style.maxWidth = '0px';
-                            } else {
-                                popoverContent.style.maxWidth = '23rem';
-                            }
                             setSearchParams((prev) =>
                                 omitQueryParams(prev, [
                                     "userIdToShowDetails",
