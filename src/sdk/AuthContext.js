bc672fa08d98e5864b540cb65641507fa9c4b0fd
xander <xandercheung0@gmail.com>
2023-11-07 19:27:02 +0800

    fix(treegrid): not use iframe

--- src/sdk/AuthContext.js
+++ src/sdk/AuthContext.js
@@ -196,6 +196,18 @@ export const AuthProvider = ({ children }) => {
         [state.token]
     );
 
+    // request TreeGrid api
+    const treeGridRequest = useCallback((url, param, callback) => {
+        authFetch(url, {
+            method: "POST",
+            body: new URLSearchParams(`Data=${ param }`),
+        }).then((response) => response.json())
+            .then((data) => {
+                callback(data)
+            });
+        return true
+    }, [authFetch]);
+
     const signin = useCallback(async (email, href) => {
         try {
             const auth = getAuth();
@@ -375,6 +387,7 @@ export const AuthProvider = ({ children }) => {
             getUser,
             getAuthorizationToken,
             hasPermission,
+            treeGridRequest,
         }),
         [
             state,
@@ -387,6 +400,7 @@ export const AuthProvider = ({ children }) => {
             getUser,
             getAuthorizationToken,
             hasPermission,
+            treeGridRequest,
         ]
     );
     return <Provider value={ providerValue }>{ children }</Provider>;
