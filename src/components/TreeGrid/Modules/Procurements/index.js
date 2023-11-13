393ce66f3ad876b4808d96d819d78bdf5116a093
wilson <wilson_wz@163.com>
2023-11-13 15:10:01 +0800

    Fix bug: 'Treegrid: After the payments, procurements group, an error occurs when the new data is added under the group row'

--- src/components/TreeGrid/Modules/Procurements/index.js
+++ src/components/TreeGrid/Modules/Procurements/index.js
@@ -32,10 +32,14 @@ const ProcurementList = ({ tabId }) => {
         }
 
         // Set parent id
-        let parentId = row.parentNode.parentNode.id
-        if (parentId !== undefined) {
-            row.parentNode.id = parentId
+        let parentId
+        if (row.parentNode.Visible === 1 && row.parentNode.Def.Name === "R") {
+            parentId = row.parentNode.id
+        } else if (row.parentNode.parentNode.Visible === 1 && row.parentNode.parentNode.Def.Name === "R") {
+            parentId = row.parentNode.parentNode.id
         }
+        // Override the current value with the default value
+        row.parentNode.id = parentId === undefined ? "0" : parentId
     }
 
     events.OnPasteRow = function (G, row, col, val) {
